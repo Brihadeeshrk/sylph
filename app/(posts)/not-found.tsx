e65ad33ddb6b3160745099100fac68c81a29be14
briha <hello@briha.xyz>
2025-01-14 23:13:44 +0530

    feat: add 404 pages to make it a more complete website

---
+++ app/(posts)/not-found.tsx
@@ -0,0 +1,34 @@
+import { DeployButton } from "@/components/deploy";
+import { Footer } from "@/components/footer";
+import Link from "@/components/link";
+import * as FadeIn from "@/components/motion/staggers/fade";
+
+const Spacer = () => <div style={{ marginTop: "24px" }} />;
+
+export default function NotFound() {
+  return (
+    <FadeIn.Container>
+      <FadeIn.Item>
+        <div className="flex justify-between">
+          <div>
+            <h1>404</h1>
+            <h2>This article does not exist</h2>
+          </div>
+        </div>
+      </FadeIn.Item>
+      <Spacer />
+      <FadeIn.Item>
+        <p>
+          The article you're looking for doesn't exist. Click here to go back{" "}
+          <Link href="/" text="home" underline />
+        </p>
+      </FadeIn.Item>
+
+      <Spacer />
+      <FadeIn.Item>
+        <Footer />
+      </FadeIn.Item>
+      <DeployButton />
+    </FadeIn.Container>
+  );
+}
